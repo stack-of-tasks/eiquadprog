2a65eaed1580b9276f32b152f721e6915cca8146
Gabriele Buondonno <gbuondon@laas.fr>
2019-09-16 15:09:48 +0200

    [eiquadprog] Remove print

--- include/eiquadprog/eiquadprog.hpp
+++ include/eiquadprog/eiquadprog.hpp
@@ -366,7 +366,6 @@ namespace Eigen {
             /* QPP is infeasible */
             // FIXME: unbounded to raise
             q = iq;
-            std::cerr<<"QP unbounded\n";
             return inf;
         }
         /* case (ii): step in dual space */
