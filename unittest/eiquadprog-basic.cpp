129e05a5fe78630337f30861085358b07dd37727
Gabriele Buondonno <gbuondon@laas.fr>
2019-09-16 11:46:43 +0200

    [unittest] First draft

---
+++ unittest/eiquadprog-basic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <Eigen/Core>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE ( BOOST_TEST_MODULE )
+
+BOOST_AUTO_TEST_CASE ( test_qp )
+{
+  std::cout << "--- Test QP ---" << std::endl;
+
+  
+  BOOST_CHECK(true);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END ()
+
