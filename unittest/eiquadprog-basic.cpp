f8ed8f375be9df230ad3c1311f2f7f28c9656988
Gabriele Buondonno <gbuondon@laas.fr>
2019-09-16 14:45:35 +0200

    [unittest] [eiquadprog-basic] test_full

--- unittest/eiquadprog-basic.cpp
+++ unittest/eiquadprog-basic.cpp
@@ -182,5 +182,52 @@ BOOST_AUTO_TEST_CASE ( test_inequality_constraints )
   BOOST_CHECK(x.isApprox(solution));
 }
 
+// min ||x-x_0||^2, x_0 = (1 1)^T
+//    s.t.
+// x[1] = 5 - x[0]
+// x[1] >= 3
+
+BOOST_AUTO_TEST_CASE ( test_full )
+{
+  Eigen::MatrixXd Q(2,2);
+  Q.setZero();
+  Q(0,0) = 1.0;
+  Q(1,1) = 1.0;
+
+  Eigen::VectorXd C(2);
+  C(0) = -1.;
+  C(1) = -1.;
+
+  Eigen::MatrixXd Aeq(2,1);
+  Aeq(0,0) = 1.;
+  Aeq(1,0) = 1.;
+
+  Eigen::VectorXd Beq(1);
+  Beq(0) = -5.;
+
+  Eigen::MatrixXd Aineq(2,1);
+  Aineq.setZero();
+  Aineq(1,0) = 1.;
+
+  Eigen::VectorXd Bineq(1);
+  Bineq(0) = -3.;
+
+  Eigen::VectorXd x(2);
+  Eigen::VectorXi activeSet;
+  size_t activeSetSize;
+
+  Eigen::VectorXd solution(1);
+  solution(0) = 2.;
+  solution(1) = 3.;
+
+  double val = 1.5;
+
+  double out = Eigen::solve_quadprog(Q, C, Aeq, Beq, Aineq, Bineq, x, activeSet, activeSetSize);
+
+  BOOST_CHECK_CLOSE(out,val,1e-6);
+
+  BOOST_CHECK(x.isApprox(solution));
+}
+
 BOOST_AUTO_TEST_SUITE_END ()
 
