0c7c1c93a2f6aaa07105df8328203b0e469c88df
Gabriele Buondonno <gbuondon@laas.fr>
2019-09-16 20:57:36 +0200

    [unittest] [eiquadprog-fast] test_biased

--- unittest/eiquadprog-fast.cpp
+++ unittest/eiquadprog-fast.cpp
@@ -58,5 +58,50 @@ BOOST_AUTO_TEST_CASE ( test_unbiased )
   BOOST_CHECK(x.isApprox(solution));
 }
 
+// min ||x-x_0||^2, x_0 = (1 1)^T
+
+BOOST_AUTO_TEST_CASE ( test_biased )
+{
+  EiquadprogFast qp;
+  qp.reset(2,0,0);
+
+  Eigen::MatrixXd Q(2,2);
+  Q.setZero();
+  Q(0,0) = 1.0;
+  Q(1,1) = 1.0;
+
+  Eigen::VectorXd C(2);
+  C(0) = -1.;
+  C(1) = -1.;
+
+  Eigen::MatrixXd Aeq(2,0);
+
+  Eigen::VectorXd Beq(0);
+
+  Eigen::MatrixXd Aineq(2,0);
+
+  Eigen::VectorXd Bineq(0);
+
+  Eigen::VectorXd x(2);
+  Eigen::VectorXi activeSet(0);
+  size_t activeSetSize;
+
+  Eigen::VectorXd solution(2);
+  solution(0) = 1.;
+  solution(1) = 1.;
+
+  double val = -1.;
+
+  EiquadprogFast_status expected = EIQUADPROG_FAST_OPTIMAL;
+
+  EiquadprogFast_status status = qp.solve_quadprog(Q, C, Aeq, Beq, Aineq, Bineq, x);
+
+  BOOST_CHECK(status==expected);
+
+  BOOST_CHECK_CLOSE(qp.getObjValue(),val,1e-6);
+
+  BOOST_CHECK(x.isApprox(solution));
+}
+
 BOOST_AUTO_TEST_SUITE_END ()
 
