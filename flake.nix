f4055d846d04bbf763f9a080073f5eb2036645e0
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-14 15:36:18 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,43 +1,25 @@
 {
   description = "C++ reimplementation of eiquadprog";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.eiquadprog;
-            eiquadprog = pkgs.eiquadprog.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
-                root = ./.;
-                fileset = pkgs.lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-            });
+    inputs.gepetto.lib.mkFlakoboros inputs (
+      { lib, ... }:
+      {
+        overrideAttrs.eiquadprog = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./include
+              ./package.xml
+              ./src
+              ./tests
+            ];
           };
         };
-    };
+      }
+    );
 }
